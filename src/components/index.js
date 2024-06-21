7f18625577e100e631bf8ea44c267f4bc4009c38
Your Name <guptamadhav1729@gmail.com>
2024-06-21 16:40:37 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,27 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "../components/Container"
+import Logo from "./Logo";
+import LogoutBtn from "../components/Header"
+import RTE from "./RTE.JSX";
+import Signup from "../pages/Signup"
+import Login from "../pages/Login";
+import Post from './Post'
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard"
+import AuthLayout from './AuthLayout'
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Post,
+    PostForm,
+    PostCard,
+    AuthLayout
+}
