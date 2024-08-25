f75c5015c4e4b48cb9e4e159a2e9679e72297c0b
Your Name <guptamadhav1729@gmail.com>
2024-08-25 17:01:14 +0530

    debug rte

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { Editor } from "@tinymce/tinymce-react";
 import { Controller } from "react-hook-form";
-import conf from "../conf/conf";
+import conf from "../conf/conf.js";
 
 export default function RTE({ name, control, label, defaultValue = "" }) {
   return (
     <div className="w-full">
       {label && <label className="inline-block mb-1 pl-1">{label}</label>}
-      
+
       <Controller
         name={name || "content"}
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
-            apiKey={conf.tinymceApiKey}
+            apiKey="7zqkmtnoquj21wa20mem61ngyve1j1amprxz1xtmgsdmpox3"
             
             init={{
               plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount linkchecker',
